96e84a6e206f845dee05d852efaec04b0cc80b52
VadimLik <162626046+VadimLik@users.noreply.github.com>
2024-05-01 10:30:31 +0300

    second
    У классов с конструкторами переменных, эти переменные помечены как final

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class KoscheiTheDeathless {
 
-    private Ocean1 ocean;
+    private  Ocean1 ocean;
 
     public String getRulesByDeth() {
         return "На свете есть океан , " + ocean.toString();
